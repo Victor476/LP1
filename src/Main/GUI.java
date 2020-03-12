1b43e611b6f4dcff1912ca8e2c3c2a0e98b80ab9
a2021064 <a2021064@debian>
2020-03-12 15:27:09 -0300

    exercicio 1

--- src/Main/GUI.java
+++ src/Main/GUI.java
@@ -1,56 +1,100 @@
 package Main;
 
+import java.awt.BorderLayout;
+
+import java.awt.Color;
+
 import java.awt.Container;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
+
 import javax.swing.JFrame;
+
 import javax.swing.JLabel;
+
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
+
  *
+
  * @author radames
+
  */
-class GUI extends JFrame {
+
+class GUI extends JFrame{
+
     Container cp;
-    JLabel lbValorA = new JLabel("Valor A");
-    JLabel lbValorB = new JLabel("Valor B");
-    JLabel lbValorC = new JLabel("Resposta");
-    JLabel lbNada= new JLabel("");
-    JTextField tfValorA = new JTextField(10);
-    JTextField tfValorB = new JTextField(10);
-    JTextField tfValorC = new JTextField(10);
-    JButton btCalcular = new JButton("Calcule");
+
+    JPanel pnA = new JPanel();
+
+    JPanel pnB = new JPanel();
+
+    JPanel pnC = new JPanel();
+
+    JPanel pnD = new JPanel();
+
+    JPanel pnE = new JPanel();
+
+    JLabel lbA = new JLabel("Media de uma dixciplina");
+    JLabel lbB = new JLabel("notas");
+    JTextField tfNota1 = new JTextField(5);
+    JTextField tfNota2 = new JTextField(5);
+    JTextField tfNota3 = new JTextField(5);
+    JTextField tfNota4 = new JTextField(5);
+    JButton btCalcular = new JButton("Calcular");
+
+   
+
     public GUI() {
-        cp = getContentPane();
-        cp.setLayout(new GridLayout(4,2));
+
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        cp = getContentPane();
+
+        cp.setLayout(new GridLayout(7,5));
         
-        cp.add(lbValorA);
-        cp.add(tfValorA);
-        cp.add(lbValorB);
-        cp.add(tfValorB);
-        cp.add(lbNada);
-        cp.add(btCalcular);
-        cp.add(lbValorC);
-        cp.add(tfValorC);
+
+        cp.add(pnA, BorderLayout.NORTH);
+
+        cp.add(pnB,BorderLayout.SOUTH);
+
+        cp.add(pnC,BorderLayout.WEST);
+
+        cp.add(pnD,BorderLayout.EAST);
+
+        cp.add(pnE,BorderLayout.CENTER);
+
+     
+        pnA.setBackground(Color.red);
+
+        pnB.setBackground(Color.BLUE);
+
+        pnC.setBackground(Color.yellow);
+
+        pnD.setBackground(Color.CYAN);
+
+       
+
+       
+
+        pnA.add(lbA);
+        pnB.add(lbB);
+        pnB.add(tfNota1);
+        pnB.add(tfNota2);
+        pnB.add(tfNota3);
+        pnB.add(tfNota4);
+        pnC.add(btCalcular);
         
-        setSize(300,300);
+        setSize(600,300);
+
+        setLocationRelativeTo(null);//centraliza
+
         setVisible(true);
-        
-        btCalcular.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                double a = Double.valueOf(tfValorA.getText());
-                double b = Double.valueOf(tfValorB.getText());
-                double resposta = a+b;
-                tfValorC.setText(String.valueOf(resposta));
-            }
-        });
-        
-    }   
-    
-    
+
+       
+
+    }    
+
 }
